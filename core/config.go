8e9fdf003ce69178696509ce7c51ff3888b99939
Kaio Delphino <dash.zet@gmail.com>
2022-11-11 00:48:49 -0300

    feat: add GetConfig

--- core/config.go
+++ core/config.go
@@ -8,6 +8,14 @@ import (
 	"github.com/cosmopool/artifex/git"
 )
 
+var gitCmd = git.Git{}
+var options = make([]Option, 0, 30)
+var config = Config{GitCmd: &gitCmd, Options: &options}
+
+func GetConfig() *Config {
+	return &config
+}
+
 type Option struct {
 	Name  string
 	Value string
