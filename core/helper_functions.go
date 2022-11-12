cf08109228d74c2c49e54c7d1112d8087f041165
Kaio Delphino <dash.zet@gmail.com>
2022-11-12 15:38:56 -0300

    feat: add helper functions

--- core/helper_functions.go
+++ core/helper_functions.go
@@ -3,6 +3,8 @@ package core
 import (
 	"fmt"
 	"strings"
+
+	"github.com/cosmopool/artifex/logger"
 )
 
 // GetTemplate returns a `configuration` template prefix and suffix.
@@ -17,4 +19,54 @@ func GetTemplate(template string) (prefix, suffix string, err error) {
 		err = fmt.Errorf("Not found '%s' word", separator)
 	}
 	return
+}
+
+func removeDoubleSlash(path string) string {
+	temp := path
+	for strings.Contains(temp, "//") {
+		temp = strings.ReplaceAll(temp, "//", "/")
+	}
+	return temp
+}
+
+// sanitizePath add a trailing '/' if non is present to `path`
+func sanitizePath(path string) string {
+	isValidPath := strings.HasPrefix(path, "/")
+	if isValidPath {
+		return path
+	} else {
+		path = "/" + path
+		return path
+	}
+}
+
+// MakePath add current dir path to `dir`
+func MakePath(dir string) string {
+	config := GetConfig()
+
+	currentDir := config.CurrentDir
+	if currentDir == "" {
+		log := logger.GetLogger()
+		log.Fatalln("Could not get current dir path.")
+	}
+
+	path := currentDir + sanitizePath(dir)
+	path = strings.ReplaceAll(path, "\n", "")
+	return removeDoubleSlash(path)
+}
+
+// MakePath add current dir path to `dir`
+func MakeFilePath(dir, fileTemplate string) string {
+	path := sanitizePath(dir) + sanitizePath(fileTemplate)
+	path = strings.ReplaceAll(path, "\n", "")
+	return removeDoubleSlash(path)
+}
+
+func ArgsContainsCommand(array []string, value string) bool {
+	for _, element := range array {
+		if element == value {
+			return true
+		}
+	}
+	return false
 }
