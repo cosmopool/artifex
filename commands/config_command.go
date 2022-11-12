2865bdc2cee523880e75a32cffe672703900fa09
Kaio Delphino <dash.zet@gmail.com>
2022-11-12 15:40:01 -0300

    feat: use Println from cmd

--- commands/config_command.go
+++ commands/config_command.go
@@ -1,8 +1,6 @@
 package commands
 
 import (
-	"fmt"
-
 	"github.com/cosmopool/artifex/core"
 	"github.com/spf13/cobra"
 )
@@ -31,52 +29,52 @@ var configCmd = &cobra.Command{
 	Long: `Configure the layers path, interfaces and implementation names and 
 	more. All the configuration the application needs to run`,
 	Run: func(cmd *cobra.Command, args []string) {
-		configRepository(repositoryConfig)
-		configRepositoryImplementation(repositoryConfig)
-		configRepositoryInterface(repositoryConfig)
-		listConfig()
+		configRepository(cmd)
+		configRepositoryImplementation(cmd)
+		configRepositoryInterface(cmd)
+		listConfig(cmd)
 	},
 }
 
-func listConfig() {
+func listConfig(cmd *cobra.Command) {
 	if *shouldListConfig {
 		options := *config.Options
 		for _, option := range options {
-			fmt.Println(option.Name, option.Value)
+			cmd.Println(option.Name, option.Value)
 		}
 	}
 }
 
-func configRepository(shouldConfig *bool) {
-	if *shouldConfig {
-		fmt.Print("The implementation path for repository (e.g. /src/infra/repositories/): ")
+func configRepository(cmd *cobra.Command) {
+	if *repositoryConfig {
+		cmd.Print("The implementation path for repository (e.g. /src/infra/repositories/): ")
 		str := core.ReadTerminal()
 		c := *config
 		c.SetOption(core.REPOSITORY_IMPLEMENTATION_PATH, str)
-		fmt.Println()
+		cmd.Println()
 	}
 }
 
-func configRepositoryInterface(shouldConfig *bool) {
-	if *shouldConfig {
-		fmt.Println("Write a exemple of a REPOSITORY INTERFACE name")
-		// fmt.Println("It must be a repository named 'Config' or 'config'")
-		fmt.Print("(e.g. 'ConfigRepositoryInterface'): ")
+func configRepositoryInterface(cmd *cobra.Command) {
+	if *repositoryConfig {
+		cmd.Println("Write a exemple of a REPOSITORY INTERFACE name")
+		// cmd.Println("It must be a repository named 'Config' or 'config'")
+		cmd.Print("(e.g. 'ConfigRepositoryInterface'): ")
 		str := core.ReadTerminal()
 		c := *config
 		c.SetOption(core.REPOSITORY_INTERFACE, str)
-		fmt.Println()
+		cmd.Println()
 	}
 }
 
-func configRepositoryImplementation(shouldConfig *bool) {
-	if *shouldConfig {
-		fmt.Println("Write a exemple of a REPOSITORY IMPLEMENTATION name")
-		// fmt.Println("It must be a repository named 'Config' or 'config'")
-		fmt.Print("(e.g. 'ConfigRepositoryImplementation'): ")
+func configRepositoryImplementation(cmd *cobra.Command) {
+	if *repositoryConfig {
+		cmd.Println("Write a exemple of a REPOSITORY IMPLEMENTATION name")
+		// cmd.Println("It must be a repository named 'Config' or 'config'")
+		cmd.Print("(e.g. 'ConfigRepositoryImplementation'): ")
 		str := core.ReadTerminal()
 		c := *config
 		c.SetOption(core.REPOSITORY_IMPLEMENTATION, str)
-		fmt.Println()
+		cmd.Println()
 	}
 }
