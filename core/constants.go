e2e6527254639019c184795bf36a8d1f47874a2d
Kaio Delphino <dash.zet@gmail.com>
2022-11-13 20:29:19 -0300

    feat: use simplified git options

--- core/constants.go
+++ core/constants.go
@@ -1,30 +1,27 @@
 package core
 
 const (
-	// DOMAIN_PATH         = "artifex.config.path.domain"
-	// EXTERNAL_PATH       = "artifex.config.path.external"
-	// INFRASTRUCTURE_PATH = "artifex.config.path.infrastructure"
-	// USECASE_PATH    = "artifex.config.path.usecase"
-	// REPOSITORY_PATH = "artifex.config.path.repository"
-	// DATASOURCE_PATH = "artifex.config.path.datasource"
+	FILE_EXTENSION = "artifex.file.extension"
 
-	FILE_EXTENSION = "artifex.config.file.extension"
+	// DOMAIN_PATH         = "artifex.path.domain"
+	// EXTERNAL_PATH       = "artifex.path.external"
+	// INFRASTRUCTURE_PATH = "artifex.path.infrastructure"
 
-	USECASE_IMPLEMENTATION_PATH    = "artifex.config.implementation.path.usecase"
-	REPOSITORY_IMPLEMENTATION_PATH = "artifex.config.implementation.path.repository"
-	DATASOURCE_IMPLEMENTATION_PATH = "artifex.config.implementation.path.datasource"
+	USECASE_IMPLEMENTATION_PATH    = "artifex.path.implementation.usecase"
+	REPOSITORY_IMPLEMENTATION_PATH = "artifex.path.implementation.repository"
+	DATASOURCE_IMPLEMENTATION_PATH = "artifex.path.implementation.datasource"
 
-	USECASE_INTERFACE_PATH    = "artifex.config.interface.path.usecase"
-	REPOSITORY_INTERFACE_PATH = "artifex.config.interface.path.repository"
-	DATASOURCE_INTERFACE_PATH = "artifex.config.interface.path.datasource"
+	// USECASE_INTERFACE_PATH    = "artifex.path.interface.usecase"
+	REPOSITORY_INTERFACE_PATH = "artifex.path.interface.repository"
+	DATASOURCE_INTERFACE_PATH = "artifex.path.interface.datasource"
 
-	USECASE_IMPLEMENTATION    = "artifex.config.implementation.filename.usecase"
-	REPOSITORY_IMPLEMENTATION = "artifex.config.implementation.filename.repository"
-	DATASOURCE_IMPLEMENTATION = "artifex.config.implementation.filename.datasource"
+	USECASE_IMPLEMENTATION    = "artifex.template.implementation.usecase"
+	REPOSITORY_IMPLEMENTATION = "artifex.template.implementation.repository"
+	DATASOURCE_IMPLEMENTATION = "artifex.template.implementation.datasource"
 
-	USECASE_INTERFACE    = "artifex.config.interface.filename.usecase"
-	REPOSITORY_INTERFACE = "artifex.config.interface.filename.repository"
-	DATASOURCE_INTERFACE = "artifex.config.interface.filename.datasource"
+	// USECASE_INTERFACE    = "artifex.template.interface.usecase"
+	REPOSITORY_INTERFACE = "artifex.template.interface.repository"
+	DATASOURCE_INTERFACE = "artifex.template.interface.datasource"
 )
 
 func ConfigList() []string {
@@ -32,9 +29,6 @@ func ConfigList() []string {
 		// DOMAIN_PATH,
 		// EXTERNAL_PATH,
 		// INFRASTRUCTURE_PATH,
-		// USECASE_PATH,
-		// REPOSITORY_PATH,
-		// DATASOURCE_PATH,
 		USECASE_IMPLEMENTATION_PATH,
 		REPOSITORY_IMPLEMENTATION_PATH,
 		DATASOURCE_IMPLEMENTATION_PATH,
@@ -42,9 +36,9 @@ func ConfigList() []string {
 		REPOSITORY_IMPLEMENTATION,
 		DATASOURCE_IMPLEMENTATION,
 		REPOSITORY_INTERFACE,
-		USECASE_INTERFACE,
+		// USECASE_INTERFACE,
 		DATASOURCE_INTERFACE,
-		USECASE_INTERFACE_PATH,
+		// USECASE_INTERFACE_PATH,
 		REPOSITORY_INTERFACE_PATH,
 		DATASOURCE_INTERFACE_PATH,
 	}
